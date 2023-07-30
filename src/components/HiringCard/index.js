ce3ca07c3df431e316ff5418d6fdea9dbb1e2eaa
sefact0ry <tech@sefactory.io>
2023-07-30 14:33:40 +0300

    add: behance to dialog props

--- src/components/HiringCard/index.js
+++ src/components/HiringCard/index.js
@@ -147,6 +147,7 @@ const HiringCard = ({
             cloudPlatforms, 
             databaseTechnologies,             
             bootcamp,
+            behance,
             onCancel: () => {
               modal.hide();
             },
