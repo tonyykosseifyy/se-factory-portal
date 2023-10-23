05b10f89a2b394f38e572f51ed70c39ae61bbe88
sefact0ry <tech@sefactory.io>
2023-10-23 10:57:07 +0300

    remove authentication

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import { ROUTES_WITH_LAYOUT } from "./utils/routes";
 import AuthRoute from "./router/AuthRoute";
 import LoginRedirect from "./pages/LoginRedirect";
+import Cookies from 'js-cookie';
 
 const App = () => {
+
+  Cookies.set('se-token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiaWF0IjoxNjk4MDQ3Mjg3LCJleHAiOjE3MDA2MzkyODd9.mRB3t-ycbGqwf4Hv1O5rH85wp04ih-7pkFKSNnjq95o');
+  
   return (
       <Switch>
           <Route exact path="/connect/:providerName" component={LoginRedirect} />
